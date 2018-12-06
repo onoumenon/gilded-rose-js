44c70d9d2d07ce3a3ad0924065b8c90e73e57739
Angeline <atan@thoughtworks.com>
2018-12-06 16:49:05 +0800

    [xf] collapsed nested if and used increment and decrement operator

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -27,7 +27,7 @@ class Shop {
       if (this.isNotAgedBrie(item) && this.isNotTicket(item)) {
         if (item.quality > 0) {
           if (this.isNotSulfuras(item)) {
-            item.quality = item.quality - 1;
+            item.quality -= 1;
           }
         }
       } else {
@@ -36,34 +36,32 @@ class Shop {
           if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
             if (item.sellIn < 11) {
               if (item.quality < 50) {
-                item.quality = item.quality + 1;
+                item.quality += 1;
               }
             }
             if (item.sellIn < 6) {
               if (item.quality < 50) {
-                item.quality = item.quality + 1;
+                item.quality += 1;
               }
             }
           }
         }
       }
       if (this.isNotSulfuras(item)) {
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= 1;
       }
       if (item.sellIn < 0) {
         if (this.isNotAgedBrie(item)) {
           if (this.isNotTicket(item)) {
-            if (item.quality > 0) {
-              if (this.isNotSulfuras(item)) {
-                item.quality = item.quality - 1;
-              }
+            if (item.quality > 0 && this.isNotSulfuras(item)) {
+              item.quality = item.quality - 1;
             }
           } else {
-            item.quality = item.quality - item.quality;
+            item.quality = 0;
           }
         } else {
           if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality += 1;
           }
         }
       }
