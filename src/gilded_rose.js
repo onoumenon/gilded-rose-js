beb125aa0601a0cdaa5f7945729cfaf861e6d302
Angeline <atan@thoughtworks.com>
2018-12-06 16:38:53 +0800

    [delph] change for loop to for of loop

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,63 +11,59 @@ class Shop {
     this.items = items;
   }
 
-  isNotAgedBrie(i) {
-    return this.items[i].name != "Aged Brie";
+  isNotAgedBrie(item) {
+    return item.name != "Aged Brie";
   }
 
-  isNotTicket(i) {
-    return this.items[i].name != "Backstage passes to a TAFKAL80ETC concert";
+  isNotTicket(item) {
+    return item.name != "Backstage passes to a TAFKAL80ETC concert";
   }
 
-  isNotSulfuras(i) {
-    return this.items[i].name != "Sulfuras, Hand of Ragnaros";
+  isNotSulfuras(item) {
+    return item.name != "Sulfuras, Hand of Ragnaros";
   }
   updateQuality() {
-    //loop though the
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.isNotAgedBrie(i) && this.isNotTicket(i)) {
-        if (this.items[i].quality > 0) {
-          if (this.isNotSulfuras(i)) {
-            this.items[i].quality = this.items[i].quality - 1;
+    for (let item of this.items) {
+      if (this.isNotAgedBrie(item) && this.isNotTicket(item)) {
+        if (item.quality > 0) {
+          if (this.isNotSulfuras(item)) {
+            item.quality = item.quality - 1;
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
           }
         }
       }
-      if (this.isNotSulfuras(i)) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (this.isNotSulfuras(item)) {
+        item.sellIn = item.sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.isNotAgedBrie(i)) {
-          if (this.isNotTicket(i)) {
-            if (this.items[i].quality > 0) {
-              if (this.isNotSulfuras(i)) {
-                this.items[i].quality = this.items[i].quality - 1;
+      if (item.sellIn < 0) {
+        if (this.isNotAgedBrie(item)) {
+          if (this.isNotTicket(item)) {
+            if (item.quality > 0) {
+              if (this.isNotSulfuras(item)) {
+                item.quality = item.quality - 1;
               }
             }
           } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+            item.quality = item.quality - item.quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
           }
         }
       }
